a70e66485cfde6e7080ae6f3526098930b1241af
Sergio Oller <sergioller@gmail.com>
2014-11-15 11:28:53 +0100

    Improve testsuite and dependency tracking
         - Update requirements.txt to reflect real metashare-3.0.1 dependencies.
         - Update django from 1.3.5 to 1.3.7 (still has a patch added to it)
         - Update httplib2 from 0.7.2 to 0.9
         - New install-dependencies.sh script:
            - Use a virtualenv and pip to download and install all python
              dependencies. Virtualenv is the recommended way for working
              with Python projects.
            - Remove the lib/ directory.
            - Remove the Python bundled file (more recent 2.7.8 is downloaded
              if required).
            - Fix additions to sys.path
         - Add support for Travis Continuous Integration Platform
            - Add a run-testsuite.sh script to run the full testsuite
            - Upgrade selenium to latest version to have compatibility with latest
              firefox version.

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -9,8 +9,8 @@ echo "Checking python version..."
 EXPECTED_PYTHON_VERSION="Python 2.7"
 
 # Check which python version to use
-if [ -e $BASEDIR/opt/bin/python ] ; then
-  PYTHON=$BASEDIR/opt/bin/python
+if [ -e "$BASEDIR/opt/bin/python" ] ; then
+  PYTHON="$BASEDIR/opt/bin/python"
   echo "Using locally installed python version $PYTHON"
 else
   PYTHON=python
@@ -25,13 +25,16 @@ else
   if [ "$PYTHON" == "python" ] ; then
     echo "expected $EXPECTED_PYTHON_VERSION, but found $PYTHON_VERSION"
     echo "trying to install a local version in $BASEDIR/opt"
-
-    cd $BASEDIR/installable-packages
-    tar xjf Python-2.7.2.tar.bz2
-    cd Python-2.7.2
-    ./configure --prefix=$BASEDIR/opt
-    make
-    make install
+    cd "$BASEDIR/installable-packages"
+    echo "Downloading python:"
+    wget "https://www.python.org/ftp/python/2.7.8/Python-2.7.8.tgz" || exit 1
+    echo "Verifying python"
+    md5sum -c "Python-2.7.8.sig" || exit 1
+    tar xzf "Python-2.7.8.tgz" || exit 1
+    cd "Python-2.7.8" || exit 1
+    ./configure --prefix="$BASEDIR/opt" || exit 1
+    make || exit 1
+    make install || exit 1
   else
     echo "expected $EXPECTED_PYTHON_VERSION, but found $PYTHON_VERSION in local install"
     echo "something is messed up, aborting."
@@ -39,9 +42,30 @@ else
   fi  
 fi
 
-echo
-echo
-echo "Installation of META-SHARE dependencies complete."
 if [ "$PYTHON" != "python" ] ; then
   echo "Python was installed locally -- make sure to include $BASEDIR/opt/bin at the beginning of your PATH!"
 fi
+
+echo "Create the virtual environment for package installation:"
+# Select current version of virtualenv:
+VENV_VERSION="1.11.6"
+VENV_DIR="${BASEDIR}/venv"
+URL_BASE="http://pypi.python.org/packages/source/v/virtualenv"
+
+echo "Download virtualenv..."
+wget "$URL_BASE/virtualenv-${VENV_VERSION}.tar.gz"
+tar xzf "virtualenv-${VENV_VERSION}.tar.gz"
+echo "Create the virtual environment:"
+"$PYTHON" "virtualenv-${VENV_VERSION}/virtualenv.py" "${VENV_DIR}"
+# Don't need this anymore.
+rm -rf "virtualenv-${VENV_VERSION}" "virtualenv-${VENV_VERSION}.tar.gz"
+
+echo "Python virtual environment created"
+
+echo "Install metashare python dependencies"
+"${VENV_DIR}/bin/pip" install -r "${BASEDIR}/requirements.txt"
+
+echo
+echo
+echo "Installation of META-SHARE dependencies complete."
+
