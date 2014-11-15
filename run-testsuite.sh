67bf6c5746e084b67eb2fc8e6005fe3dd2b72efe
Sergio Oller <sergioller@gmail.com>
2014-11-15 11:28:53 +0100

    Improve testsuite and dependency tracking
         - Update requirements.txt to reflect real metashare-3.0.1 dependencies.
         - Update django from 1.3.5 to 1.3.7 (still has a patch added to it)
         - Update httplib2 from 0.7.2 to 0.9
         - New install-dependencies.sh script:
            - Use a virtualenv and pip to download and install all python
              dependencies. Virtualenv is the recommended way for working
              with Python projects.
            - Remove the lib/ directory.
            - Remove the Python bundled file (more recent 2.7.8 is downloaded
              if required).
            - Fix additions to sys.path
         - Add support for Travis Continuous Integration Platform
            - Add a run-testsuite.sh script to run the full testsuite
            - Upgrade selenium to latest version to have compatibility with latest
              firefox version.

---
+++ run-testsuite.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# This script runs the metashare testsuite. It is written
+# IMPORTANT: This script creates a new firefox profile in which the popup
+# blocker is disabled.
+# This profile is not removed automatically (FIXME) because firefox does not
+# offer a command line to remove profiles (as far as I know)
+# This means that if you run this on your own computer you will need to
+# remove the firefox profile "firefox_profile" by yourself.
+# How to remove a Firefox profile?
+# You only need to close firefox and open it. A dialog
+# will appear for you to choose the profile you want, with an option
+# to delete any profile you have. Please delete the "firefox_profile".
+# Your settings are usually saved in the "default" profile, so do not remove
+# that profile.
+
+# Copy test configuration
+mkdir -p "storage" || exit 1
+echo $(pwd)
+cp metashare/local_settings.test metashare/local_settings.py || exit 1
+cp metashare/initial_data.test.json metashare/initial_data.json || exit 1
+
+# Start Xvfb
+export DISPLAY=":99.0"
+Xvfb "$DISPLAY" -ac -screen 0 1024x768x16 &
+sleep 10
+
+# Prepare firefox profile
+rm -rf "$PWD/firefox_profile"
+firefox -CreateProfile "firefox_profile $PWD/firefox_profile" || exit 1
+tar xzf firefox_profile_template.tgz || exit 1
+cp -pr firefox_profile_template/* "$PWD/firefox_profile" || exit 1
+rm -rf firefox_profile_template
+
+
+# Initialize database
+source venv/bin/activate || exit 1
+python metashare/manage.py syncdb --noinput || exit 1
+rm metashare/initial_data.json || exit 1
+
+# Run testsuite:
+metashare/start-solr.sh || exit 1
+sleep 10
+python metashare/manage.py test --selenium || exit 1
+deactivate || exit 1
+metashare/stop-solr.sh || exit 1
+rm -rf "$PWD/firefox_profile"
+
