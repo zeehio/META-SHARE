d85536de1e14cc91ca2c0df40af151182fd1a601
Sergio Oller <sergioller@gmail.com>
2015-03-22 13:31:19 +0100

    Allow travis to push documentation using a token. Comment .travis.yml

--- misc/test-ci/autoupdate-docs.sh
+++ misc/test-ci/autoupdate-docs.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-git config user.name "Travis-CI"
-git config user.email "travis@travis-ci.org"
-
 # Build the docs:
 (cd misc/docs/ && make html)
-# Move the output to gh-pages
+cd ..
+git clone "https://${GH_AUTH}@${GH_REF}" metashare_docs || exit 1
+cd metashare_docs
 git checkout gh-pages
+git config user.name "Travis-CI"
+git config user.email "travis@travis-ci.org"
 # Replace the old web pages by the new ones
 rm -rf dev
-mv misc/docs/_build/html/ dev
-git add --all dev
-git commit -m "Automatic update of documentation"
-#git remote add rworigin "https://${GH_AUTH}@${GH_REF}"
-#git push rworigin gh-pages
+mv ../META-SHARE/misc/docs/_build/html/ dev || exit 1
+git add --all dev || exit 1
+git commit -m "Automatic update of documentation" || exit 1
 git push origin gh-pages
+
