e3a6ec620045be81f296dac9fd7ff0464731698b
Sergio Oller <sergioller@gmail.com>
2015-09-11 22:37:45 +0200

    Update to latest python 2.7.10

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -18,6 +18,8 @@ fi
 
 PYTHON_VERSION=$($PYTHON --version 2>&1)
 PREFIX_MATCH=$(expr "$PYTHON_VERSION" : "\($EXPECTED_PYTHON_VERSION\)")
+LATEST_PYTHON="2.7.10"
+LATEST_PYTHON_MD5="d7547558fd673bd9d38e2108c6b42521"
 
 if [ "$PREFIX_MATCH" != "" ] ; then
   echo "$PYTHON_VERSION found, OK."
@@ -25,16 +27,17 @@ else
   if [ "$PYTHON" == "python" ] ; then
     echo "expected $EXPECTED_PYTHON_VERSION, but found $PYTHON_VERSION"
     echo "trying to install a local version in $BASEDIR/opt"
-    wget "https://www.python.org/ftp/python/2.7.9/Python-2.7.9.tgz" || exit 1
+    # sudo apt-get build-dep python
+    wget "https://www.python.org/ftp/python/$LATEST_PYTHON/Python-$LATEST_PYTHON.tgz" || exit 1
     echo "Verifying python"
-    echo "5eebcaa0030dc4061156d3429657fb83  Python-2.7.9.tgz" | md5sum -c - || exit 1
-    tar xzf "Python-2.7.9.tgz" || exit 1
-    cd "Python-2.7.9" || exit 1
+    echo "$LATEST_PYTHON_MD5  Python-$LATEST_PYTHON.tgz" | md5sum -c - || exit 1
+    tar xzf "Python-$LATEST_PYTHON.tgz" || exit 1
+    cd "Python-$LATEST_PYTHON" || exit 1
     ./configure --prefix="$BASEDIR/opt" || exit 1
     make || exit 1
     make install || exit 1
     cd ".."
-    rm -rf "Python-2.7.9"
+    rm -rf "Python-$LATEST_PYTHON"
     PYTHON="$BASEDIR/opt/bin/python"
   else
     echo "expected $EXPECTED_PYTHON_VERSION, but found $PYTHON_VERSION in local install"
@@ -46,13 +49,14 @@ fi
 echo "Create the virtual environment for package installation:"
 # Select current version of virtualenv:
 VENV_DIR="${BASEDIR}/venv"
-VENV_VERSION="12.0.7"
-URL_BASE="http://pypi.python.org/packages/source/v/virtualenv"
+VENV_VERSION="13.1.2"
+VENV_MD5="b989598f068d64b32dead530eb25589a"
+URL_BASE="https://pypi.python.org/packages/source/v/virtualenv"
 
 echo "Download virtualenv..."
 wget "$URL_BASE/virtualenv-${VENV_VERSION}.tar.gz" || exit 1
 echo "Verify virtualenv..."
-echo "e08796f79d112f3bfa6653cc10840114  virtualenv-12.0.7.tar.gz" | md5sum -c - || exit 1
+echo "${VENV_MD5}  virtualenv-${VENV_VERSION}.tar.gz" | md5sum -c - || exit 1
 echo "Extracting virtualenv"
 tar xzf "virtualenv-${VENV_VERSION}.tar.gz" || exit 1
 echo "Create the virtual environment:"
@@ -62,6 +66,7 @@ rm -rf "virtualenv-${VENV_VERSION}" "virtualenv-${VENV_VERSION}.tar.gz"
 
 echo "Python virtual environment created"
 
+#sudo apt-get install libsqlite3-dev
 echo "Install metashare python dependencies"
 "${VENV_DIR}/bin/pip" install -r "${BASEDIR}/requirements.txt" || exit 1
 
--- run-tests.sh
+++ run-tests.sh
@@ -5,7 +5,7 @@ if [ ! -d "venv" ]; then
 fi
 
 source venv/bin/activate || exit 1
-pip install coveralls || exit 1
+pip install codecov || exit 1
 deactivate || exit 1
 
 ./misc/test-ci/run-testsuite.sh || exit 1
@@ -13,7 +13,7 @@ deactivate || exit 1
 ./misc/test-ci/multitest.sh || exit 1
 
 source venv/bin/activate || exit 1
-coveralls || exit 1
+codecov || exit 1
 deactivate || exit 1
 
 
